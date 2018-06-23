5c8ea718476c63e0799ff583b035094d9f31761c
Naoki Nishizawa <nnishiza@amazon.co.jp>
2018-06-23 20:54:16 +0900

    Drop unnecessary code

--- day4/step0/bootpack.c
+++ day4/step0/bootpack.c
@@ -4,12 +4,8 @@ void Main() {
   char *p = (void *)0x000a0000;
   int i;
   for( i = 0x0000; i < 0xffff; i++ ) {
-    p[i] = (char)90;
+    p[i] = (char)10;
   }
 
-  while(1) {}
-  //
-  //io_hlt();
-
-  //while(1) { io_hlt(); }
+  while(1) { io_hlt(); }
 }
